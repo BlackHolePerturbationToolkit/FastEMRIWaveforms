a25af3cedafa6b31913c5ac84604d1ffc3bd6547
Niels Warburton <niels.warburton@ucd.ie>
2020-02-20 21:04:42 +0000

    Correct comment

--- inspiral/src/FluxInspiral.cc
+++ inspiral/src/FluxInspiral.cc
@@ -170,7 +170,7 @@ int main (void) {
 			break;
 		}
 		
-		// Output format: p, e, Phi_phi, Phi_r
+		// Output format: t, p, e, Phi_phi, Phi_r
       	printf ("%.5e %.5e %.5e %.5e %.5e\n", t, y[0], y[1], y[2], y[3]);
     }
 	high_resolution_clock::time_point t2 = high_resolution_clock::now();
