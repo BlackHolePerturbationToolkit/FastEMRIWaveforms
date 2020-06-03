40c5baa8d36a54d647e855a40e542bbe75ca5a1a
mikekatz04 <mikekatz04@gmail.com>
2020-06-03 17:56:26 -0500

    inspiral changes from Niels

--- inspiral/src/FluxInspiral.cc
+++ inspiral/src/FluxInspiral.cc
@@ -5,7 +5,13 @@
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_odeiv2.h>
 #include <gsl/gsl_sf_ellint.h>
-#include <memory.h>
+#include <algorithm>
+
+#include <hdf5.h>
+#include <hdf5_hl.h>
+
+#include <complex>
+#include <cmath>
 
 #include "Interpolant.h"
 #include "FluxInspiral.hh"
@@ -15,10 +21,26 @@
 #include <sstream>
 #include <vector>
 #include <chrono>
+#include <iomanip>      // std::setprecision
+#include <cstring>
 
 using namespace std;
 using namespace std::chrono;
 
+// Definitions needed for Mathematicas CForm output
+#define Power(x, y)     (pow((double)(x), (double)(y)))
+#define Sqrt(x)         (sqrt((double)(x)))
+#define Pi              M_PI
+
+
+// This code assumes the data is formated in the following way
+const int Ne = 33;
+const int Ny = 50;
+
+const double SolarMassInSeconds = 4.925e-6;
+const double YearInSeconds 		= 60*60*25*365.25;
+
+const int DENSE_STEPPING = 1;
 
 // Define elliptic integrals that use Mathematica's conventions
 double EllipticK(double k){
@@ -54,8 +76,25 @@ int func (double t, const double y[], double f[], void *params){
 	double p = y[0];
 	double e = y[1];
 
-	double Edot = -epsilon*interps->Edot->eval(p-2*e, e);
-	double Ldot = -epsilon*interps->Ldot->eval(p-2*e, e);
+	double y1 = log((p -2.*e - 2.1));
+
+	// Need to evaluate 4 different elliptic integrals here. Cache them first to avoid repeated calls.
+	double EllipE 	= EllipticE(4*e/(p-6.0+2*e));
+	double EllipK 	= EllipticK(4*e/(p-6.0+2*e));;
+	double EllipPi1 = EllipticPi(16*e/(12.0 + 8*e - 4*e*e - 8*p + p*p), 4*e/(p-6.0+2*e));
+	double EllipPi2 = EllipticPi(2*e*(p-4)/((1.0+e)*(p-6.0+2*e)), 4*e/(p-6.0+2*e));
+
+	double Omega_phi = (2*Power(p,1.5))/(Sqrt(-4*Power(e,2) + Power(-2 + p,2))*(8 + ((-2*EllipPi2*(6 + 2*e - p)*(3 + Power(e,2) - p)*Power(p,2))/((-1 + e)*Power(1 + e,2)) - (EllipE*(-4 + p)*Power(p,2)*(-6 + 2*e + p))/(-1 + Power(e,2)) +
+          (EllipK*Power(p,2)*(28 + 4*Power(e,2) - 12*p + Power(p,2)))/(-1 + Power(e,2)) + (4*(-4 + p)*p*(2*(1 + e)*EllipK + EllipPi2*(-6 - 2*e + p)))/(1 + e) + 2*Power(-4 + p,2)*(EllipK*(-4 + p) + (EllipPi1*p*(-6 - 2*e + p))/(2 + 2*e - p)))/
+        (EllipK*Power(-4 + p,2))));
+
+	double yPN = pow(Omega_phi,2./3.);
+
+	double EdotPN = (96 + 292*Power(e,2) + 37*Power(e,4))/(15.*Power(1 - Power(e,2),3.5)) * pow(yPN, 5);
+	double LdotPN = (4*(8 + 7*Power(e,2)))/(5.*Power(-1 + Power(e,2),2)) * pow(yPN, 7./2.);
+
+	double Edot = -epsilon*(interps->Edot->eval(y1, e)*pow(yPN,6.) + EdotPN);
+	double Ldot = -epsilon*(interps->Ldot->eval(y1, e)*pow(yPN,9./2.) + LdotPN);
 
 	double pdot = (-2*(Edot*Sqrt((4*Power(e,2) - Power(-2 + p,2))/(3 + Power(e,2) - p))*(3 + Power(e,2) - p)*Power(p,1.5) + Ldot*Power(-4 + p,2)*Sqrt(-3 - Power(e,2) + p)))/(4*Power(e,2) - Power(-6 + p,2));
 
@@ -64,15 +103,9 @@ int func (double t, const double y[], double f[], void *params){
 	 (-1 + Power(e,2))*Ldot*Sqrt(-3 - Power(e,2) + p)*(12 + 4*Power(e,2) - 8*p + Power(p,2)))/
 	(e*(4*Power(e,2) - Power(-6 + p,2))*p));
 
-	// Need to evaluate 4 different elliptic integrals here. Cache them first to avoid repeated calls.
-	double EllipE 	= EllipticE(4*e/(p-6.0+2*e));
-	double EllipK 	= EllipticK(4*e/(p-6.0+2*e));;
-	double EllipPi1 = EllipticPi(16*e/(12.0 + 8*e - 4*e*e - 8*p + p*p), 4*e/(p-6.0+2*e));
-	double EllipPi2 = EllipticPi(2*e*(p-4)/((1.0+e)*(p-6.0+2*e)), 4*e/(p-6.0+2*e));
 
-	double Phi_phi_dot 	= (2*Power(p,1.5))/(Sqrt(-4*Power(e,2) + Power(-2 + p,2))*(8 + ((-2*EllipPi2*(6 + 2*e - p)*(3 + Power(e,2) - p)*Power(p,2))/((-1 + e)*Power(1 + e,2)) - (EllipE*(-4 + p)*Power(p,2)*(-6 + 2*e + p))/(-1 + Power(e,2)) +
-          (EllipK*Power(p,2)*(28 + 4*Power(e,2) - 12*p + Power(p,2)))/(-1 + Power(e,2)) + (4*(-4 + p)*p*(2*(1 + e)*EllipK + EllipPi2*(-6 - 2*e + p)))/(1 + e) + 2*Power(-4 + p,2)*(EllipK*(-4 + p) + (EllipPi1*p*(-6 - 2*e + p))/(2 + 2*e - p)))/
-        (EllipK*Power(-4 + p,2))));
+
+	double Phi_phi_dot 	= Omega_phi;
 
 	double Phi_r_dot 	= (p*Sqrt((-6 + 2*e + p)/(-4*Power(e,2) + Power(-2 + p,2)))*Pi)/
    (8*EllipK + ((-2*EllipPi2*(6 + 2*e - p)*(3 + Power(e,2) - p)*Power(p,2))/((-1 + e)*Power(1 + e,2)) - (EllipE*(-4 + p)*Power(p,2)*(-6 + 2*e + p))/(-1 + Power(e,2)) +
@@ -88,18 +121,13 @@ int func (double t, const double y[], double f[], void *params){
 }
 
 
-void load_and_interpolate_flux_data(struct interp_params *interps){
-
-
-}
-
 
 FluxCarrier::FluxCarrier()
 {
     interps = new interp_params;
 
     // Load and interpolate the flux data
-	ifstream Flux_file("inspiral/data/Flux.dat");
+	ifstream Flux_file("data/FluxNewMinusPNScaled.dat");
 
 	// Load the flux data into arrays
 	string Flux_string;
@@ -116,6 +144,13 @@ FluxCarrier::FluxCarrier()
 		Edots.push_back(Edot);
 		Ldots.push_back(Ldot);
 	}
+
+	// Remove duplicate elements (only works if ys are perfectly repeatined with no round off errors)
+	sort( ys.begin(), ys.end() );
+	ys.erase( unique( ys.begin(), ys.end() ), ys.end() );
+
+	sort( es.begin(), es.end() );
+	es.erase( unique( es.begin(), es.end() ), es.end() );
 
 	Interpolant *Edot_interp = new Interpolant(ys, es, Edots);
 	Interpolant *Ldot_interp = new Interpolant(ys, es, Ldots);
@@ -147,51 +182,60 @@ FLUXHolder run_FLUX(double t0, double M, double mu, double p0, double e0, double
     //high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
 	double tmax = 1e7;
+    double dt = 10.0;
 
 
 	// Initial values
 	// TODO do we want to set initial phases here?
 	double y[4] = { p0, e0, 0.0, 0.0 };
 
-	// Initialize the ODE solver
-		gsl_odeiv2_system sys = {func, NULL, 4, &interps};
-		const gsl_odeiv2_step_type *T = gsl_odeiv2_step_rk8pd;
+    // Initialize the ODE solver
+  	gsl_odeiv2_system sys = {func, NULL, 4, &interps};
+    const gsl_odeiv2_step_type *T = gsl_odeiv2_step_rk8pd;
 
-		gsl_odeiv2_step *s 		= gsl_odeiv2_step_alloc (T, 4);
-		gsl_odeiv2_control *c 	= gsl_odeiv2_control_y_new (err, err);
-		gsl_odeiv2_evolve *e 	= gsl_odeiv2_evolve_alloc (4);
+    gsl_odeiv2_step *step 			= gsl_odeiv2_step_alloc (T, 4);
+    gsl_odeiv2_control *control 	= gsl_odeiv2_control_y_new (1e-10, 0);
+    gsl_odeiv2_evolve *evolve 		= gsl_odeiv2_evolve_alloc (4);
 
+	// Compute the inspiral
 	double t = t0;
-	double h = 1.0;
-	int ind=0;
+	double h = dt;
+	double t1 = tmax;
+    int ind = 0;
+	if(DENSE_STEPPING) t1 = dt;
 	while (t < tmax){
-				int status = gsl_odeiv2_evolve_apply (e, c, s, &sys, &t, tmax, &h, y);
+      	int status = gsl_odeiv2_evolve_apply (evolve, control, step, &sys, &t, t1, &h, y);
+		if(DENSE_STEPPING) t1 += dt;
+      	if (status != GSL_SUCCESS){
+       		printf ("error, return value=%d\n", status);
+          	break;
+        }
 
-				if (status != GSL_SUCCESS){
-					printf ("error, return value=%d\n", status);
-						break;
-				}
-		// Stop the inspiral when close to the separatrix
-		if(y[0] - 6 -2*y[1] < 0.1){
-			//cout << "# Separatrix reached: exiting inspiral" << endl;
-			break;
-		}
 
 		flux_out.add_point(t*Msec, y[0], y[1], y[2], y[3]); // adds time in seconds
+
+        double p 		= y[0];
+        double e 		= y[1];
+
+        ind++;
+        // Stop the inspiral when close to the separatrix
+        if(p - 6 -2*e < 0.1){
+            cout << "# Separatrix reached: exiting inspiral" << endl;
+            break;
+        }
 
 		// Output format: t, p, e, Phi_phi, Phi_r
 				//printf ("%.5e %.5e %.5e %.5e %.5e\n", flux_out.t_arr.push_back(t), flux_out.p_arr.push_back(y[0]), flux_out.e_arr.push_back(y[1]), y[2], y[3]);
 
-				ind++;
-		}
+	}
 	flux_out.length = ind;
 	//high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
 	//	duration<double> time_span = duration_cast<duration<double> >(t2 - t1);
 
-		gsl_odeiv2_evolve_free (e);
-		gsl_odeiv2_control_free (c);
-		gsl_odeiv2_step_free (s);
+        gsl_odeiv2_evolve_free (evolve);
+        gsl_odeiv2_control_free (control);
+        gsl_odeiv2_step_free (step);
 		//cout << "# Computing the inspiral took: " << time_span.count() << " seconds." << endl;
 		return flux_out;
 
