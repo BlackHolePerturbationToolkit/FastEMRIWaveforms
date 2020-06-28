614af8f54cdd3a2518b8c6f4283a9f4081bebc58
mikekatz04 <mikekatz04@gmail.com>
2020-06-28 18:36:21 -0500

    removed slow waveform folder

--- SlowFluxWaveform/README.md
+++
@@ -1,7 +0,0 @@
-# Slow waveform calculation
-
-The code in this directory computes the waveform slowly. The code:
-- densely samples the phase space trajectory and the waveform phases
-- Interpolate the waveform amplitudes for each lmn-mode and computes the waveform by evaluating each of these and summing.
-
-Compile the code using `scons`.
--- SlowFluxWaveform/include/SpinWeightedSphericalHarmonics.hh
+++
@@ -1,9 +0,0 @@
-#ifndef __SPIN_WEIGHTED_SPHER_HARM__
-#define __SPIN_WEIGHTED_SPHER_HARM__
-
-
-void get_spin_weighted_spher_harm(std::complex<double>*harms_out, int *l_arr, int *m_arr, double theta, double phi, int num);
-
-std::complex<double> SpinWeightedSphericalHarmonic (int l, int m, double theta, double phi);
-
-#endif  // __SPIN_WEIGHTED_SPHER_HARM__
