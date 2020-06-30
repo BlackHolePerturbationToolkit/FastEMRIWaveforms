f4eb67a3ed6e4f94a1b9c985fcbec358e124d80f
mikekatz04 <mikekatz04@gmail.com>
2020-06-29 20:18:27 -0500

    doc intro updated

--- few/summation/__init__.py
+++ few/summation/__init__.py
@@ -1,6 +1,6 @@
 """
 The summation package houses codes used for combining amplitude and phase
 information over the course of an inspiral. It does contain GPU accelerated
-options.
+options. It deals with the overall summation of quantities seen in :eq:`emri_wave_eq`.
 
 """
