65318b6b5e37c86b688132bca8ac3837627c3e9c
mikekatz04 <mikekatz04@gmail.com>
2020-07-08 19:20:25 -0500

    bug fix

--- src/pyinterp2damp.pyx
+++ src/pyinterp2damp.pyx
@@ -2,7 +2,7 @@ import numpy as np
 cimport numpy as np
 from libcpp.string cimport string
 
-from utils.pointer_adjust import pointer_adjust
+from few.utils.pointer_adjust import pointer_adjust
 
 assert sizeof(int) == sizeof(np.int32_t)
 
