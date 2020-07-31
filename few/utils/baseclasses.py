1de1977271ad2c4ed142a8e80296226c5de9c444
mikekatz04 <mikekatz04@gmail.com>
2020-07-31 15:29:12 -0500

    amplitude base added

--- few/utils/baseclasses.py
+++ few/utils/baseclasses.py
@@ -296,7 +296,7 @@ class TrajectoryBase(ABC):
         returns:
             2D double np.ndarray: t, p, e, Phi_phi, Phi_r, flux with shape:
                 (params, traj length).
-                
+
         raises:
             NotImplementedError: The child class does not have this method.
 
@@ -501,3 +501,60 @@ class SummationBase(ABC):
         self.sum(*args_in)
 
         return self.waveform
+
+
+class AmplitudeBase(ABC):
+    """Base class used for amplitude modules.
+
+    This class provides a common flexible interface to various amplitude
+    implementations. Specific arguments to each amplitude module can be found
+    with each associated module discussed below.
+
+    args:
+        pad_output (bool, optional): Add zero padding to the waveform for time
+            between plunge and observation time. Default is False.
+
+    """
+
+    def __init__(self, **kwargs):
+        pass
+
+    @classmethod
+    def get_amplitudes(self, *args, **kwargs):
+        """Amplitude Generator
+
+        @classmethod that requires a child class to have a get_amplitudes method.
+
+        raises:
+            NotImplementedError: The child class does not have this method.
+
+        """
+        raise NotImplementedError
+
+    def __call__(self, p, e, *args, specific_modes=None, **kwargs):
+        """Common call for Teukolsky amplitudes
+
+        This function takes the inputs the trajectory in :math:`(p,e)` as arrays
+        and returns the complex amplitude of all modes to adiabatic order at
+        each step of the trajectory.
+
+        args:
+            p (1D double numpy.ndarray): Array containing the trajectory for values of
+                the semi-latus rectum.
+            e (1D double numpy.ndarray): Array containing the trajectory for values of
+                the eccentricity.
+            *args (tuple, placeholder): Added to create future flexibility when calling different
+                amplitude modules.
+            specific_modes (list, optional): List of tuples for (l, m, n) values
+                desired modes. Default is None.
+            **kwargs (dict, placeholder): Added to create flexibility when calling different
+                amplitude modules. It is not used.
+
+        returns:
+            2D array (double): If specific_modes is None, Teukolsky modes in shape (number of trajectory points, number of modes)
+            dict: Dictionary with requested modes.
+
+
+        """
+
+        return self.get_amplitudes(p, e, *args, specific_modes=specific_modes, **kwargs)
