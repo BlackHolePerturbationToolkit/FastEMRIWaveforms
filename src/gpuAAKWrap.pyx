53e9613963c5bad7c3d17a53dc40937d000f5d6a
mikekatz04 <mikekatz04@gmail.com>
2020-10-12 12:37:12 +0200

    many updates

--- src/gpuAAKWrap.pyx
+++ src/gpuAAKWrap.pyx
@@ -7,7 +7,8 @@ from few.utils.utility import pointer_adjust
 assert sizeof(int) == sizeof(np.int32_t)
 
 cdef extern from "../include/gpuAAK.hh":
-    void get_waveform(double *hI, double *hII, double* interp_array,
+    ctypedef void* cmplx 'cmplx'
+    void get_waveform(cmplx* waveform, double* interp_array,
                   double M_phys, double mu, double lam, double qS, double phiS, double qK, double phiK, double dist,
                   int nmodes, bool mich,
                   int init_len, int out_len,
@@ -15,18 +16,17 @@ cdef extern from "../include/gpuAAK.hh":
 
 
 @pointer_adjust
-def pyWaveform(hI, hII, interp_array,
+def pyWaveform(waveform, interp_array,
               M_phys, mu, lam, qS, phiS, qK, phiK, dist,
               nmodes, mich,
               init_len, out_len,
               delta_t, h_t):
 
-    cdef size_t hI_in = hI
-    cdef size_t hII_in = hII
+    cdef size_t waveform_in = waveform
     cdef size_t interp_array_in = interp_array
     cdef size_t h_t_in = h_t
 
-    get_waveform(<double*> hI_in, <double*> hII_in, <double*> interp_array_in,
+    get_waveform(<cmplx*> waveform_in, <double*> interp_array_in,
                   M_phys, mu, lam, qS, phiS, qK, phiK, dist,
                   nmodes, mich,
                   init_len, out_len,
