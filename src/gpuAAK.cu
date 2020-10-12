8064543b95d4a0a9c83bd048ecbe3befd49e2b04
mikekatz04 <mikekatz04@gmail.com>
2020-10-12 12:49:47 +0200

    bug fix

--- src/gpuAAK.cu
+++ src/gpuAAK.cu
@@ -48,8 +48,8 @@ void d_RotCoeff(double* rot, double* n, double* L, double* S, double* nxL, doubl
   //gsl_blas_ddot(nxL,nxS,&dot);
   dot = d_dot_product(nxL,nxS);
 
-  if (norm == 0.0) norm = 1e-6;
-  
+  if (norm < 1e-6) norm = 1e-6;
+
   cosrot=dot/norm;
   //gsl_blas_ddot(L,nxS,&dot);
   dot = d_dot_product(L,nxS);
