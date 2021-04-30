5f850d03dd168dbe1629c3ab4063a9cd8e9e25af
lorenzsp <lorenzo.speri@gmail.com>
2021-04-30 12:11:08 +0200

    mismatch working

--- fd_test.py
+++ fd_test.py
@@ -3,7 +3,17 @@ from few.waveform import FastSchwarzschildEccentricFlux
 from few.utils.constants import *
 import numpy as np
 import matplotlib.pyplot as plt
-
+def shift5(arr, num, fill_value=np.nan):
+    result = np.empty_like(arr)
+    if num > 0:
+        result[:num] = fill_value
+        result[num:] = arr[:-num]
+    elif num < 0:
+        result[num:] = fill_value
+        result[:num] = arr[-num:]
+    else:
+        result[:] = arr
+    return result
 
 sum_kwargs = dict(pad_output=True)
 
@@ -14,7 +24,7 @@ few_base = FastSchwarzschildEccentricFlux(
 M = 1e6
 mu = 50
 p0 = 12
-e0 = 0.5
+e0 = 0.3
 theta= np.pi/3
 phi = np.pi/5
 dt=10
@@ -22,13 +32,14 @@ T=.9
 
 
 
-
 #%% TIME DOMAIN
 wave_22 = few_base(M, mu, p0, e0, theta, phi,T=T,dt=dt, eps=1e-2)
-freq_fft = np.fft.fftshift(np.fft.fftfreq(len(wave_22),dt))
-fft_wave = np.fft.fftshift(np.fft.fft(wave_22))*dt
+freq_fft = np.fft.fftfreq(len(wave_22),dt)
+fft_wave = np.fft.fft(wave_22)*dt
 
-sum_kwargs = dict(pad_output=True, output_type="fd")
+
+
+sum_kwargs = dict(pad_output=True, output_type="fd",f=fft_wave)
 
 wave = FastSchwarzschildEccentricFlux(sum_kwargs=sum_kwargs)
 
@@ -37,7 +48,7 @@ fd_h = wave(M,mu,p0,e0,theta,phi,T=T,dt=dt,eps=1e-2)
 f = np.arange(-1/(2*dt),+1/(2*dt),1/(len(fd_h)*dt))
 #%% mismatch
 index_nonzero = [fd_h != 0][0]
-fd_h_correct = -( np.flip(np.real(fd_h)) + 1j* np.flip(np.imag(fd_h)) )
+fd_h_correct = -np.roll( np.flip(np.real(fd_h)) + 1j* np.flip(np.imag(fd_h)), 1)
 den = np.sqrt(np.real(np.dot(np.conj(fft_wave[index_nonzero]),fft_wave[index_nonzero])) * np.real(np.dot(np.conj(fd_h_correct[index_nonzero]),fd_h_correct[index_nonzero])) )
 print("mismatch = " ,1-np.real(np.dot(np.conj(fd_h_correct[index_nonzero]) , fft_wave[index_nonzero] ) )/den)
 
@@ -50,7 +61,7 @@ plt.xlabel('f [Hz]')
 # TD model
 plt.plot(freq_fft, np.real(fft_wave), label='fft TD waveform')
 # FD model
-plt.plot(freq_fft, -np.flip(np.real(fd_h)),'--',alpha=0.9,label='FD domain waveform' )
+plt.plot(freq_fft, np.real(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
 plt.legend()
 plt.show()
 
@@ -65,7 +76,7 @@ plt.xlabel('f [Hz]')
 # TD model
 plt.plot(freq_fft, np.imag(fft_wave), label='fft TD waveform')
 # FD model
-plt.plot(f, -np.flip(np.imag(fd_h)),'--',alpha=0.9,label='FD domain waveform' )
+plt.plot(freq_fft, np.imag(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
 plt.legend()
 plt.show()
 # %%
