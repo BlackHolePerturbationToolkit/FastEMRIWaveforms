311cda346583cb503c8dced6436e8401e9150211
lorenzsp <lorenzo.speri@gmail.com>
2021-05-02 01:22:10 +0200

    good mismatches

--- fd_test.py
+++ fd_test.py
@@ -20,37 +20,42 @@ sum_kwargs = dict(pad_output=True)
 few_base = FastSchwarzschildEccentricFlux(
     sum_kwargs=sum_kwargs,
 )
+# p0=12;e0=0.3 -> mismatch =  0.000778239562794103
 
-M = 1e6
-mu = 50
-p0 = 12
-e0 = 0.2
-theta= np.pi/3
-phi = np.pi/5
+M = 1e6; mu = 50; p0 = 10; e0 = 0.7; theta= np.pi/3; phi = np.pi/5 # mismatch =  0.0014153820095872405
 dt=10
-T=1.
+T=2
 
 
-
+l=3 #2
+m=1 #1
+n=-1 #-4
 #%% TIME DOMAIN
-wave_22 = few_base(M, mu, p0, e0, theta, phi,T=T,dt=dt, eps=1e-3)
+wave_22 = few_base(M, mu, p0, e0, theta, phi,T=T,dt=dt, eps=1e-2)
 freq_fft = np.fft.fftfreq(len(wave_22),dt)
 fft_wave = np.fft.fft(wave_22)*dt
 
 
 
-sum_kwargs = dict(pad_output=True, output_type="fd",f=fft_wave)
+sum_kwargs = dict(pad_output=True, output_type="fd")
 
 wave = FastSchwarzschildEccentricFlux(sum_kwargs=sum_kwargs)
 
-fd_h = wave(M,mu,p0,e0,theta,phi,T=T,dt=dt,eps=1e-3)
+fd_h = wave(M,mu,p0,e0,theta,phi,T=T,dt=dt, eps=1e-2)
 
 f = np.arange(-1/(2*dt),+1/(2*dt),1/(len(fd_h)*dt))
 #%% mismatch
+fd_h[np.isnan(fd_h)] = 0
+print(np.sum(np.isnan(fd_h)))
+
 index_nonzero = [fd_h != 0][0]
 fd_h_correct = -np.roll( np.flip(np.real(fd_h)) + 1j* np.flip(np.imag(fd_h)), 1)
-den = np.sqrt(np.real(np.dot(np.conj(fft_wave[index_nonzero]),fft_wave[index_nonzero])) * np.real(np.dot(np.conj(fd_h_correct[index_nonzero]),fd_h_correct[index_nonzero])) )
-print("mismatch = " ,1-np.real(np.dot(np.conj(fd_h_correct[index_nonzero]) , fft_wave[index_nonzero] ) )/den)
+# check nan
+
+den = np.sqrt(np.real(np.dot(np.conj(fft_wave),fft_wave)) * np.real(np.dot(np.conj(fd_h_correct),fd_h_correct)) )
+
+print("mismatch = " ,1-np.real(np.dot(np.conj(fd_h_correct) , fft_wave ) )/den)
+
 
 # np.dot(np.real(fft_wave),-np.real(fd_h)) + np.dot(np.imag(fft_wave),-np.imag(fd_h))
 
@@ -62,7 +67,7 @@ plt.xlabel('f [Hz]')
 plt.plot(freq_fft, np.real(fft_wave), label='fft TD waveform')
 # FD model
 plt.plot(freq_fft, np.real(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
-plt.legend()
+plt.legend(loc='right')
 plt.show()
 
 
--- few/summation/fdinterp.py
+++ few/summation/fdinterp.py
@@ -47,7 +47,7 @@ except (ImportError, ModuleNotFoundError) as e:
 # for special functions
 from scipy import special
 from scipy.interpolate import CubicSpline
-
+import multiprocessing as mp
 
 # added to return element or none in list
 def find_element_in_list(element, list_element):
@@ -264,8 +264,9 @@ class FDInterpolatedModeSum(SummationBase, SchwarzschildEccentric, GPUModuleBase
         # final waveform
         h = np.zeros_like(self.frequency, dtype=complex)
 
-        # indentify where there is a turnover
-        for j, (m, n) in enumerate(zip(m_arr, n_arr)):
+        # multithreading
+        """
+        def fd_mode(j,m,n):
 
             # check turnover
             freq_mode_start = m * spline.c1[-4,0] + n * spline.c1[-3,0]
@@ -290,106 +291,7 @@ class FDInterpolatedModeSum(SummationBase, SchwarzschildEccentric, GPUModuleBase
 
                 # calculate frequency indeces and respective time of f
                 ind_1, ind_2, t_f_1, t_f_2 = self.time_frequency_map(spline_f_mode,index_star)
-                
-                """
-                # turn over index
-                index_star = index_star - 1 
 
-
-                # find t_star analytically
-                ratio = spline_f_mode.c2[0,index_star]/(3*spline_f_mode.c3[0,index_star])
-                second_ratio = spline_f_mode.c1[0,index_star]/(3*spline_f_mode.c3[0,index_star])
-                t_star = t[index_star] -ratio + np.sqrt(ratio**2 - second_ratio)
-
-                # new frequancy vector
-                Fstar = spline_f_mode(t_star)
-                new_F = np.append(f_mn[:index_star+1], Fstar + Fstar/np.abs(Fstar) * np.abs(f_mn[index_star+1:] - Fstar) )
-                new_F_spline = CubicSplineInterpolant(t, new_F)
-
-                # new t_f
-                sign_slope = spline_f_mode.c1[0,0]/np.abs(spline_f_mode.c1[0,0])
-                # HERE PROBLEM WITH THE CUBIC SPLINEInterp
-                # should throw a warning if decreasing
-                # need to put an absolute value but it might be incorrect for modes passing through zero
-                modified_t_f = CubicSpline(np.abs(new_F),t) # take axis frequency monotonically increasing
-
-                # frequency split
-                initial_frequency = f_mn[0]
-                end_frequency = f_mn[-1]
-
-
-                if (initial_frequency>end_frequency):
-                    index_single = (self.frequency<initial_frequency)*(self.frequency>end_frequency)
-
-                    if sign_slope>0:
-                        # alt imple
-                        modified2_t_f = CubicSpline((new_F),t)
-                        ind_1 = (self.frequency>end_frequency)*(self.frequency<Fstar)
-                        t_f_1 = modified2_t_f(Fstar+Fstar/np.abs(Fstar) * np.abs(self.frequency[ind_1] - Fstar))
-                        ind_2 = (self.frequency>initial_frequency)*(self.frequency<Fstar)
-                        t_f_2 = modified2_t_f(self.frequency[ind_2])
-
-                        index_double = (self.frequency>initial_frequency)*(self.frequency<Fstar)
-                        t_f_sing = modified_t_f(np.abs(Fstar+Fstar/np.abs(Fstar) * np.abs(self.frequency[index_single] - Fstar)))
-                    else:
-                        modified2_t_f = CubicSpline(-(new_F),t) # 
-                        
-                        ind_1 = (self.frequency<end_frequency)*(self.frequency>Fstar)
-                        ind_2 = (self.frequency<initial_frequency)*(self.frequency>Fstar)
-                        t_f_1 = modified2_t_f(-(Fstar+Fstar/np.abs(Fstar) * np.abs(self.frequency[ind_1] - Fstar)))
-                        t_f_2 = modified2_t_f(-(self.frequency[ind_2]))
-
-                        index_double = (self.frequency<end_frequency)*(self.frequency>Fstar)
-                        t_f_sing = modified_t_f(np.abs(self.frequency[index_single]))
-
-
-                if (initial_frequency<end_frequency):
-                    index_single = (self.frequency>initial_frequency)*(self.frequency<end_frequency)
-                    
-                    if sign_slope>0:
-
-                        modified2_t_f = CubicSpline((new_F),t)
-
-                        ind_1 = (self.frequency>initial_frequency)*(self.frequency<Fstar)
-                        ind_2 = (self.frequency>end_frequency)*(self.frequency<Fstar)
-                        t_f_2 = modified2_t_f((Fstar+Fstar/np.abs(Fstar) * np.abs(self.frequency[ind_2] - Fstar)))
-                        t_f_1 = modified2_t_f((self.frequency[ind_1]))
-
-                        #index_double = (self.frequency>end_frequency)*(self.frequency<Fstar)
-                        #t_f_sing = modified_t_f(np.abs(self.frequency[index_single]))
-                    else:
-                        modified2_t_f = CubicSpline(-(new_F),t) # 
-                        
-                        ind_1 = (self.frequency<end_frequency)*(self.frequency>Fstar)
-                        ind_2 = (self.frequency<initial_frequency)*(self.frequency>Fstar)
-                        t_f_1 = modified2_t_f(-(Fstar+Fstar/np.abs(Fstar) * np.abs(self.frequency[ind_1] - Fstar)))
-                        t_f_2 = modified2_t_f(-(self.frequency[ind_2]))
-
-                        index_double = (self.frequency<initial_frequency)*(self.frequency>Fstar)
-                        t_f_sing = modified_t_f(np.abs(Fstar+Fstar/np.abs(Fstar) * np.abs(self.frequency[index_single] - Fstar)))
-
-                
-                # plt.plot(t,f_mn); plt.axhline(self.frequency[index_single][0]); plt.show()
-                # plt.plot(t,f_mn); plt.axhline(self.frequency[index_single][-1]); plt.show()
-                # import matplotlib.pyplot as plt
-                # print(spline(t_f_sing))
-
-                
-                # double contribution
-                t_f_first_half = modified_t_f( np.abs(self.frequency[index_double])  )
-                t_f_second_half = modified_t_f(np.abs(Fstar+Fstar/np.abs(Fstar) * np.abs(self.frequency[index_double] - Fstar)))
-                
-                # Waveform Amplitudes
-
-                h_two_contr = [(spline(t_two)[j] + 1j* spline(t_two)[num_teuk_modes+j] )*ylms[j]*\
-                                    self.waveform_spa_factors(fdot_spline(t_two), fddot_spline(t_two))*\
-                                    np.exp( 1j*( 2*np.pi*f* t_two  - ( m * spline(t_two)[-2] + n * spline(t_two)[-1]) ) )
-                                for t_two,f in zip([t_f_first_half,t_f_second_half,t_f_sing],[self.frequency[index_double],self.frequency[index_double],self.frequency[index_single]])
-                                ]
-                """
-
-                #print(sign_slope,(initial_frequency<end_frequency) )
-                #breakpoint()
                 h_contr = [(spline(t_two)[j] + 1j* spline(t_two)[num_teuk_modes+j] )*ylms[j]*\
                     self.waveform_spa_factors(fdot_spline(t_two), fddot_spline(t_two))*\
                     np.exp( 1j*( 2*np.pi*f* t_two  - ( m * spline(t_two)[-2] + n * spline(t_two)[-1]) ) )
@@ -398,9 +300,18 @@ class FDInterpolatedModeSum(SummationBase, SchwarzschildEccentric, GPUModuleBase
 
                 h[ind_1] += h_contr[0]
                 h[ind_2] += h_contr[1]
-                #h[index_double] += np.sum(h_two_contr[:2],axis=0)
-                #h[index_single] += h_two_contr[2]
 
+                # negative contribution
+                if m!=0:
+
+                    h_contr = [(spline(t_two)[j] - 1j* spline(t_two)[num_teuk_modes+j] )*ylms[j+num_teuk_modes]*\
+                        self.waveform_spa_factors(-fdot_spline(t_two), -fddot_spline(t_two))*\
+                        np.exp( 1j*( 2*np.pi*f* t_two + ( m * spline(t_two)[-2] + n * spline(t_two)[-1]) ) )
+                    for t_two,f in zip([np.flip(t_f_1),np.flip(t_f_2)],[self.frequency[np.flip(ind_1)],self.frequency[np.flip(ind_2)]])
+                    ]
+
+                    h[np.flip(ind_1)] += h_contr[0]
+                    h[np.flip(ind_2)] += h_contr[1]
 
             else:
 
@@ -430,8 +341,100 @@ class FDInterpolatedModeSum(SummationBase, SchwarzschildEccentric, GPUModuleBase
                 
                 Exp_neg = np.exp(1j*(2*np.pi* f_neg * t_neg  + (m * spline(t_neg)[-2] + n * spline(t_neg)[-1]) ) )
 
+                #breakpoint()
+
                 h[index_positive_f] += Teuk_sph_pos*Exp_pos
                 h[index_negative_f] += Teuk_sph_neg*Exp_neg
+        
+        from multiprocessing.pool import ThreadPool
+
+        pool = ThreadPool(8)
+
+        inp = [(j, m, n) for j, (m, n) in enumerate(zip(m_arr, n_arr))]
+        L = pool.starmap(fd_mode, [(j, m, n) for j, (m, n) in enumerate(zip(m_arr, n_arr))])
+        """
+        
+        
+        # indentify where there is a turnover
+        for j, (m, n) in enumerate(zip(m_arr, n_arr)):
+
+            # check turnover
+            freq_mode_start = m * spline.c1[-4,0] + n * spline.c1[-3,0]
+            index_star = find_element_in_list(True, list(freq_mode_start*(m * spline.c1[-4,:] + n * spline.c1[-3,:])<0.))
+            
+            # frequency mode
+            f_mn = m * f_phi + n * f_r
+            spline_f_mode = CubicSplineInterpolant(t, f_mn, use_gpu=self.use_gpu)
+
+            min_f_mn = np.min(np.abs(f_mn))
+            max_f_mn = np.max(np.abs(f_mn))
+
+            # fdot
+            fdot_spline = CubicSplineInterpolant(t, np.asarray(spline_f_mode.deriv(t)), use_gpu=self.use_gpu)
+
+            # fddot
+            fddot_spline = CubicSplineInterpolant(t, np.asarray(fdot_spline.deriv(t)), use_gpu=self.use_gpu)
+
+            if index_star is not None:
+                print(m,n)
+                print('there is a turn-over')
+
+                # calculate frequency indeces and respective time of f
+                ind_1, ind_2, t_f_1, t_f_2 = self.time_frequency_map(spline_f_mode,index_star)
+
+                h_contr = [(spline(t_two)[j] + 1j* spline(t_two)[num_teuk_modes+j] )*ylms[j]*\
+                    self.waveform_spa_factors(fdot_spline(t_two), fddot_spline(t_two))*\
+                    np.exp( 1j*( 2*np.pi*f* t_two  - ( m * spline(t_two)[-2] + n * spline(t_two)[-1]) ) )
+                for t_two,f in zip([t_f_1,t_f_2],[self.frequency[ind_1],self.frequency[ind_2]])
+                ]
+
+                h[ind_1] += h_contr[0]
+                h[ind_2] += h_contr[1]
+
+                # negative contribution
+                if m!=0:
+
+                    h_contr = [(spline(t_two)[j] - 1j* spline(t_two)[num_teuk_modes+j] )*ylms[j+num_teuk_modes]*\
+                        self.waveform_spa_factors(-fdot_spline(t_two), -fddot_spline(t_two))*\
+                        np.exp( 1j*( 2*np.pi*f* t_two + ( m * spline(t_two)[-2] + n * spline(t_two)[-1]) ) )
+                    for t_two,f in zip([np.flip(t_f_1),np.flip(t_f_2)],[self.frequency[np.flip(ind_1)],self.frequency[np.flip(ind_2)]])
+                    ]
+
+                    h[np.flip(ind_1)] += h_contr[0]
+                    h[np.flip(ind_2)] += h_contr[1]
+
+            else:
+
+                # time of f
+                t_of_f = CubicSplineInterpolant(f_mn, t, use_gpu=self.use_gpu)
+                
+                # frequency
+                index_positive_f = (self.frequency>min_f_mn)*(self.frequency<max_f_mn)
+                index_negative_f = (self.frequency<-min_f_mn)*(self.frequency>-max_f_mn)
+                f_pos = self.frequency[index_positive_f]
+                f_neg = self.frequency[index_negative_f]
+
+                # time evluated quantities
+                t_pos = t_of_f(f_pos)
+                t_neg = t_of_f(-f_neg) # same as np.flip(t_pos)
 
 
+                # teukolsky modes
+                Teuk_sph_pos = (spline(t_pos)[j] + 1j* spline(t_pos)[num_teuk_modes+j] )*ylms[j]*\
+                    self.waveform_spa_factors(fdot_spline(t_pos), fddot_spline(t_pos))
+
+                Teuk_sph_neg = (spline(t_neg)[j] - 1j* spline(t_neg)[num_teuk_modes+j] )*ylms[j+num_teuk_modes]*\
+                    self.waveform_spa_factors(-fdot_spline(t_neg), -fddot_spline(t_neg))
+
+                
+                Exp_pos = np.exp(1j*(2*np.pi* f_pos * t_pos  - (m * spline(t_pos)[-2] + n * spline(t_pos)[-1]) ) )
+                
+                Exp_neg = np.exp(1j*(2*np.pi* f_neg * t_neg  + (m * spline(t_neg)[-2] + n * spline(t_neg)[-1]) ) )
+
+                #breakpoint()
+
+                h[index_positive_f] += Teuk_sph_pos*Exp_pos
+                h[index_negative_f] += Teuk_sph_neg*Exp_neg
+        
+        
         self.waveform = h
