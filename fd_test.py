36b208109ea1d9c3fb43732d7c249d43a161b6ce
lorenzsp <lorenzo.speri@gmail.com>
2021-05-11 10:28:13 +0200

    fixed cubic spling

--- fd_test.py
+++ fd_test.py
@@ -3,17 +3,7 @@ from few.waveform import FastSchwarzschildEccentricFlux
 from few.utils.constants import *
 import numpy as np
 import matplotlib.pyplot as plt
-def shift5(arr, num, fill_value=np.nan):
-    result = np.empty_like(arr)
-    if num > 0:
-        result[:num] = fill_value
-        result[num:] = arr[:-num]
-    elif num < 0:
-        result[num:] = fill_value
-        result[:num] = arr[-num:]
-    else:
-        result[:] = arr
-    return result
+
 
 sum_kwargs = dict(pad_output=True)
 
@@ -22,18 +12,20 @@ few_base = FastSchwarzschildEccentricFlux(
 )
 # p0=12;e0=0.3 -> mismatch =  0.000778239562794103
 
-M = 1e6; mu = 50; p0 = 10; e0 = 0.7; theta= np.pi/3; phi = np.pi/5 # mismatch =  0.0014153820095872405
-M = 1e6; mu = 50; p0 = 17.4; e0 = 0.7; theta= np.pi/3; phi = np.pi/5 # mismatch =  0.14
+M = 1e6; mu = 40; p0 = 10; e0 = 0.7; theta= np.pi/3; phi = np.pi/5 # mismatch =  0.0014153820095872405
+#M = 1e6; mu = 50; p0 = 17.4; e0 = 0.7; theta= np.pi/3; phi = np.pi/5 # mismatch =  0.02257016504821807
 
-dt=10
-T=4
+# mism 0.0024275870323797744
+dt=5
+T=0.5
 
 
-l=3 #2
+l=2 #2
 m=1 #1
-n=-1 #-4
+n=-4 #-4
+
 #%% TIME DOMAIN
-wave_22 = few_base(M, mu, p0, e0, theta, phi,T=T,dt=dt, eps=1e-2)
+wave_22 = few_base(M, mu, p0, e0, theta, phi,T=T,dt=dt , eps=1e-2) #mode_selection=[(l,m,n)],include_minus_m=True) #,
 freq_fft = np.fft.fftfreq(len(wave_22),dt)
 fft_wave = np.fft.fft(wave_22)*dt
 
@@ -43,7 +35,7 @@ sum_kwargs = dict(pad_output=True, output_type="fd")
 
 wave = FastSchwarzschildEccentricFlux(sum_kwargs=sum_kwargs)
 
-fd_h = wave(M,mu,p0,e0,theta,phi,T=T,dt=dt, eps=1e-2)
+fd_h = wave(M,mu,p0,e0,theta,phi,T=T,dt=dt , eps=1e-2) #,mode_selection=[(l,m,n)]) #
 
 f = np.arange(-1/(2*dt),+1/(2*dt),1/(len(fd_h)*dt))
 #%% mismatch
@@ -54,9 +46,9 @@ index_nonzero = [fd_h != 0][0]
 fd_h_correct = -np.roll( np.flip(np.real(fd_h)) + 1j* np.flip(np.imag(fd_h)), 1)
 # check nan
 
-den = np.sqrt(np.real(np.dot(np.conj(fft_wave),fft_wave)) * np.real(np.dot(np.conj(fd_h_correct),fd_h_correct)) )
+den = np.sqrt(np.real(np.dot(np.conj(fft_wave[index_nonzero]),fft_wave[index_nonzero])) * np.real(np.dot(np.conj(fd_h_correct[index_nonzero]),fd_h_correct[index_nonzero])) )
 
-print("mismatch = " ,1-np.real(np.dot(np.conj(fd_h_correct) , fft_wave ) )/den)
+print("mismatch = " ,1-np.real(np.dot(np.conj(fd_h_correct[index_nonzero]) , fft_wave[index_nonzero] ) )/den)
 
 
 # np.dot(np.real(fft_wave),-np.real(fd_h)) + np.dot(np.imag(fft_wave),-np.imag(fd_h))
