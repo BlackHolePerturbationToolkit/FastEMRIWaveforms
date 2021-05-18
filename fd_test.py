4487c4cc01af52137312649dceff5ed2af7362be
lorenzsp <lorenzo.speri@gmail.com>
2021-05-18 21:23:16 +0200

    fixed nans in the waveform

--- fd_test.py
+++ fd_test.py
@@ -1,5 +1,6 @@
 #%%
-from few.waveform import FastSchwarzschildEccentricFlux
+from few.waveform import FastSchwarzschildEccentricFlux, RunSchwarzEccFluxInspiral
+from few.utils.utility import *
 from few.utils.constants import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,19 +15,30 @@ few_base = FastSchwarzschildEccentricFlux(
 # p0=12;e0=0.3 -> mismatch =  0.000778239562794103
 
 M = 1e6; mu = 40; p0 = 10; e0 = 0.7; theta= np.pi/3; phi = np.pi/5 # mismatch =  0.0014153820095872405
-#M = 1e6; mu = 50; p0 = 17.4; e0 = 0.7; theta= np.pi/3; phi = np.pi/5 # mismatch =  0.02257016504821807
 
+#M = 1e6; mu = 50; p0 = 11.48; e0 = 0.7; theta= np.pi/3; phi = np.pi/5 # mismatch =  0.02257016504821807
+
+traj_module = RunSchwarzEccFluxInspiral()
+
+traj_args = [M, p0, e0]
+traj_kwargs = {}
+index_of_mu = 1
+
+t_out = 1.
+# run trajectory
+mu = get_mu_at_t(traj_module, t_out, traj_args)
+print(mu)
 # mism 0.0024275870323797744
 dt=10
 T=1.
 
 
 l=2 #2
-m=0 #1
-n=2 #-4
+m=1 #1
+n=-4 #-4
 
 #%% TIME DOMAIN
-wave_22 = few_base(M, mu, p0, e0, theta, phi,T=T,dt=dt,eps=1e-2)# ,mode_selection=[(l,m,n)],include_minus_m=True) #
+wave_22 = few_base(M, mu, p0, e0, theta, phi,T=T,dt=dt,mode_selection=[(l,m,n)],include_minus_m=True) #,eps=1e-2)# 
 freq_fft = np.fft.fftfreq(len(wave_22),dt)
 fft_wave = np.fft.fft(wave_22 )*dt #* signal.tukey(len(wave_22))
 
@@ -37,24 +49,30 @@ sum_kwargs = dict(pad_output=True, output_type="fd")
 
 wave = FastSchwarzschildEccentricFlux(sum_kwargs=sum_kwargs)
 
-fd_h = wave(M,mu,p0,e0,theta,phi,T=T,dt=dt,eps=1e-2)# , mode_selection=[(l,m,n)],include_minus_m=True) #
+fd_h = wave(M,mu,p0,e0,theta,phi,T=T,dt=dt,mode_selection=[(l,m,n)],include_minus_m=True) #,eps=1e-2)# , mode_selection=[(l,m,n)],include_minus_m=True) #
 
 f = np.arange(-1/(2*dt),+1/(2*dt),1/(len(fd_h)*dt))
+
+
 #%% mismatch
-fd_h[np.isnan(fd_h)] = 0
+print("nans in waveform", np.sum(np.isnan(fd_h)))
 
 fd_h_correct = -np.roll( np.flip(np.real(fd_h)) + 1j* np.flip(np.imag(fd_h)), 1)#np.sin(dt*len(wave_22)*freq_fft/4/np.pi)/np.sin(dt*freq_fft/4/np.pi)#*np.exp(-1j* (len(wave_22)-1)/2 )
-index_nonzero = [fd_h_correct != 0.0][0]
+index_nonzero = [np.abs(fd_h_correct) !=complex(0.0)][0]
 
 # check nan
 
 den = np.sqrt(np.real(np.dot(np.conj(fft_wave[index_nonzero]),fft_wave[index_nonzero])) * np.real(np.dot(np.conj(fd_h_correct[index_nonzero]),fd_h_correct[index_nonzero])) )
-print('den',den,'index',index_nonzero)
+print('den',den,'index',np.sum(index_nonzero))
 print("mismatch = " ,1-np.real(np.dot(np.conj(fd_h_correct[index_nonzero]) , fft_wave[index_nonzero] ) )/den)
+
+den = np.sqrt(np.real(np.dot(np.conj(fft_wave[index_nonzero]),fft_wave[index_nonzero])) * np.real(np.dot(np.conj(fd_h_correct[index_nonzero]),fd_h_correct[index_nonzero])) )
+print('den',den,'index',np.sum(index_nonzero))
+print("mismatch = " ,1-np.real(np.dot(np.abs(fd_h_correct[index_nonzero]) , np.abs(fft_wave[index_nonzero]) ) )/den)
 
 
 # np.dot(np.real(fft_wave),-np.real(fd_h)) + np.dot(np.imag(fft_wave),-np.imag(fd_h))
-
+"""
 # figure
 plt.figure()
 plt.ylabel(r'Re $\tilde{h}(f)$')
@@ -80,4 +98,28 @@ plt.plot(freq_fft, np.imag(fft_wave), label='fft TD waveform')
 plt.plot(freq_fft, np.imag(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
 plt.legend()
 plt.show()
+"""
 # %%
+
+# figure
+plt.figure()
+plt.ylabel(r'Ang $\tilde{h}(f)$')
+plt.xlabel('f [Hz]')
+# TD model
+plt.plot(freq_fft, np.angle(fft_wave), label='fft TD waveform')
+# FD model
+plt.plot(freq_fft, np.angle(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
+plt.legend()
+plt.show()
+
+
+# figure
+plt.figure()
+plt.ylabel(r' $|\tilde{h}(f)|$')
+plt.xlabel('f [Hz]')
+# TD model
+plt.plot(freq_fft, np.abs(fft_wave), label='fft TD waveform')
+# FD model
+plt.plot(freq_fft, np.abs(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
+plt.legend()
+plt.show()
