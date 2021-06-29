eb181d7b1e81c32859ffb581883c23e8adbbac95
lorenzsp <lorenzo.speri@gmail.com>
2021-06-29 11:39:19 +0200

    new inner product

--- fd_test.py
+++ fd_test.py
@@ -30,33 +30,34 @@ mu = get_mu_at_t(traj_module, t_out, traj_args)
 print(mu)
 # mism 0.0024275870323797744
 dt=10.
-T=1#/2.5
+T=1.#/2.5
 
 
 l = 2 #2
-m = 2 #1
-n = 0 #-4
+m = 1 #1
+n = 3 #-4
 
 #%% TIME DOMAIN
-wave_22 = few_base(M, mu, p0, e0, theta, phi,T=T,dt=dt,eps=1e-2)#mode_selection=[(l,m,n)],include_minus_m=True)  , batch_size=int(1e2),mode_selection=[(l,m,n)])#,include_minus_m=True) #
-freq_fft = np.fft.fftfreq(len(wave_22),dt)
-fft_wave = np.fft.fftshift(np.fft.fft( np.real(wave_22) )*dt)
+wave_22 = few_base(M, mu, p0, e0, theta, phi,T=T,dt=dt,eps=5e-2)#,mode_selection=[(l,m,n)])mode_selection=[(l,m,n)],include_minus_m=True)  , batch_size=int(1e2),mode_selection=[(l,m,n)])#,include_minus_m=True) #
+freq_fft = np.fft.fftshift(np.fft.fftfreq(len(wave_22),dt))
+fft_wave = np.fft.fftshift(np.fft.fft(np.real(wave_22)-1j* np.imag(wave_22) )*dt)
 
 sum_kwargs = dict(pad_output=True, output_type="fd")
 
 wave = FastSchwarzschildEccentricFlux(sum_kwargs=sum_kwargs)
 
-fd_h = wave(M,mu,p0,e0,theta,phi,T=T,dt=dt,eps=1e-2)#,mode_selection=[(l,m,n)],include_minus_m=True) #,eps=1e-2)# , mode_selection=[(l,m,n)],include_minus_m=True) #
+fd_h = wave(M,mu,p0,e0,theta,phi,T=T,dt=dt,eps=5e-2)#,mode_selection=[(l,m,n)])#,eps=1e-2)#,include_minus_m=True) #,eps=1e-2)# , mode_selection=[(l,m,n)],include_minus_m=True) #
 
 f = np.arange(-1/(2*dt),+1/(2*dt),1/(len(fd_h)*dt))
 
 real_fd = np.real(fd_h + np.flip(fd_h))/2 + 1j* np.imag(fd_h - np.flip(fd_h))/2
+imag_fd = -np.imag(fd_h + np.flip(fd_h))/2 - 1j* np.real(fd_h - np.flip(fd_h))/2
 
 #%% mismatch
 
 print("nans in waveform", np.sum(np.isnan(fd_h)))
 
-fd_h_correct = real_fd #-np.roll( np.flip(np.real(fd_h)) + 1j* np.flip(np.imag(fd_h)), 1)#np.sin(dt*len(wave_22)*freq_fft/4/np.pi)/np.sin(dt*freq_fft/4/np.pi)#*np.exp(-1j* (len(wave_22)-1)/2 )
+fd_h_correct = fd_h #-np.roll( np.flip(np.real(fd_h)) + 1j* np.flip(np.imag(fd_h)), 1)#np.sin(dt*len(wave_22)*freq_fft/4/np.pi)/np.sin(dt*freq_fft/4/np.pi)#*np.exp(-1j* (len(wave_22)-1)/2 )
 
 def innprod(a,b):
     return np.real(np.dot(np.conj(a),b))
@@ -64,31 +65,33 @@ def innprod(a,b):
 # check nan
 den = np.sqrt(innprod(fft_wave,fft_wave) * innprod(fd_h_correct,fd_h_correct))
 print("mismatch = " ,1-innprod(fft_wave,fft_wave)/den)
+print("overlap = " ,innprod(fft_wave,fft_wave)/den)
 
-"""
+
 # figure
 plt.figure()
-plt.ylabel(r'Re $\tilde{h}(f)$')
+plt.ylabel(r' $|\tilde{h}(f)|$')
 plt.xlabel('f [Hz]')
 # TD model
-plt.plot( np.real(fft_wave), label='fft TD waveform')
+plt.semilogx(freq_fft, np.abs(fft_wave), label='fft TD waveform')
 # FD model
-plt.plot( np.real(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
-plt.legend(loc='right')
+plt.semilogx(freq_fft, np.abs(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
+plt.legend(loc='best')
 plt.show()
-"""
+
 
 # %%
 
-"""
+
 # figure
 plt.figure()
 plt.ylabel(r'Imag $\tilde{h}(f)$')
 plt.xlabel('f [Hz]')
 # TD model
-plt.plot(freq_fft, np.imag(fft_wave), label='fft TD waveform')
+plt.semilogx(freq_fft, np.imag(fft_wave), label='fft TD waveform')
 # FD model
-plt.plot(freq_fft, np.imag(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
+plt.semilogx(freq_fft, np.imag(fd_h_correct),'--',alpha=0.9,label='FD domain waveform' )
 plt.legend()
 plt.show()
-"""
+
+# %%
