c6ec4e1d6f4ee642991ffc77de62b2c452fe88ca
lorenzsp <lorenzo.speri@gmail.com>
2023-02-21 11:57:43 +0100

    compare fluxes and on5

--- AAK_spinning.py
+++ AAK_spinning.py
@@ -17,17 +17,23 @@ except (ModuleNotFoundError, ImportError) as e:
 
 from few.trajectory.inspiral import EMRIInspiral
 from few.utils.baseclasses import SchwarzschildEccentric, Pn5AAK, ParallelModuleBase
-from few.waveform import AAKWaveformBase
+from few.waveform import AAKWaveformBase, Pn5AAKWaveform
 from few.summation.aakwave import AAKSummation
+from few.utils.utility import get_mismatch
 
 insp_kwargs = {
     "err": 1e-10,
     "DENSE_STEPPING": 0,
-    "max_init_len": int(1e4),
+    "max_init_len": int(1e3),
     "func":"KerrEccentricEquatorial"
     }
 
-sum_kwargs = dict(use_gpu=gpu_available)
+# keyword arguments for summation generator (AAKSummation)
+sum_kwargs = {
+    "use_gpu": gpu_available,  # GPU is availabel for this type of summation
+    "pad_output": False,
+}
+
 num_threads = 16
 
 waveform_class = AAKWaveformBase(
@@ -39,13 +45,15 @@ waveform_class = AAKWaveformBase(
             num_threads=num_threads,
         )
 
+wave_generator = Pn5AAKWaveform( sum_kwargs=sum_kwargs, use_gpu=gpu_available)
+
 ####################
 # set initial parameters
 M = 5e5
 mu = 1e1
 a = 0.85
 p0 = 8.0
-e0 = 0.4
+e0 = 0.2
 Y0 = 1.0
 Phi_phi0 = 0.2
 Phi_theta0 = 1.2
@@ -61,8 +69,25 @@ dt = 1.0
 T = 1.0
 
 # new input
-secondary_spin = 1e-4
+secondary_spin = 0.0
 
+# check limit
+output1 = waveform_class(M,mu,a,p0,e0,Y0,dist,qS,phiS,qK,phiK,secondary_spin,
+        Phi_phi0=0.0,Phi_theta0=0.0,Phi_r0=0.0,mich=False,dt=dt,T=T)
+
+secondary_spin = 1e-5
+output2 = waveform_class(M,mu,a,p0,e0,Y0,dist,qS,phiS,qK,phiK,secondary_spin,
+        Phi_phi0=0.0,Phi_theta0=0.0,Phi_r0=0.0,mich=False,dt=dt,T=T)
+
+print(get_mismatch(output2.real, output1.real))
+
+plt.figure()
+plt.plot(output1.real[-100:],label='new wave')
+plt.plot(output2.real[-100:],label='AAK wave')
+plt.legend()
+plt.show()
+
+breakpoint()
 # plot as a function of dt
 dtvec = np.linspace(1,15, num=2)
 plt.figure()
