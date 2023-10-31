e06f676a4120a125f701c55f6a26f32a334c55a2
Christian Chapman-Bird <c.chapman-bird.1@research.gla.ac.uk>
2023-10-31 14:38:44 +0000

    Fix some setup.py flags

--- setup.py
+++ setup.py
@@ -265,13 +265,13 @@ if run_cuda_install:
             "gcc": ["-std=c++11", "-fopenmp", "-D__USE_OMP__"],  # '-g'],
             "nvcc": [
                 "-arch=sm_70",
-                "-gencode=arch=compute_50,code=sm_50",
-                "-gencode=arch=compute_52,code=sm_52",
-                "-gencode=arch=compute_60,code=sm_60",
-                "-gencode=arch=compute_61,code=sm_61",
+                #"-gencode=arch=compute_50,code=sm_50",
+                #"-gencode=arch=compute_52,code=sm_52",
+                #"-gencode=arch=compute_60,code=sm_60",
+                #"-gencode=arch=compute_61,code=sm_61",
                 "-gencode=arch=compute_70,code=sm_70",
-                "-gencode=arch=compute_75,code=sm_75",
-                "-gencode=arch=compute_80,code=sm_80",
+                #"-gencode=arch=compute_75,code=sm_75",
+                #"-gencode=arch=compute_80,code=sm_80",
                 "-std=c++11",
                 "--default-stream=per-thread",
                 "--ptxas-options=-v",
@@ -286,7 +286,7 @@ if run_cuda_install:
                 # "-O0",
                 # "-lineinfo",
             ],  # for debugging
-            'nvcclink': ['-arch_sm70', '--device-link', '--compiler-options', "'-fPIC'"]
+            'nvcclink': ['-arch=sm_70', '--device-link', '--compiler-options', "'-fPIC'"]
         },
         include_dirs=[numpy_include, CUDA["include"], "include"],
     )
