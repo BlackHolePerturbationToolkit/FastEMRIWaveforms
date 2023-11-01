1f0fca964ce337d98f7eca7ce10c7184c91737b5
Christian Chapman-Bird <c.chapman-bird.1@research.gla.ac.uk>
2023-11-01 16:25:28 +0000

    Cherry-pick header files

--- include/Utility.hh
+++ include/Utility.hh
@@ -1,8 +1,6 @@
 #ifndef __FUND_FREQS__
 #define __FUND_FREQS__
 
-void throw_python_error(char* str_in, int status);
-
 int sanity_check(double a, double p, double e, double Y);
 
 void KerrGeoMinoFrequencies(double* CapitalGamma_, double* CapitalUpsilonPhi_, double* CapitalUpsilonTheta_, double* CapitalUpsilonr_,
@@ -20,6 +18,12 @@ void KerrGeoEquatorialCoordinateFrequencies(double* OmegaPhi_, double* OmegaThet
 void KerrGeoCoordinateFrequenciesVectorized(double* OmegaPhi_, double* OmegaTheta_, double* OmegaR_,
                               double* a, double* p, double* e, double* x, int length);
 
+void KerrEqSpinFrequenciesCorrVectorized(double* OmegaPhi_, double* OmegaTheta_, double* OmegaR_,
+                              double* a, double* p, double* e, double* x, int length);
+
+void KerrEqSpinFrequenciesCorrection(double* deltaOmegaR_, double* deltaOmegaPhi_,
+                              double a, double p, double e, double x);
+void KerrScott(double* OmegaPhi_, double* OmegaTheta_, double* OmegaR_,double a, double p, double e, double xI);
 void SchwarzschildGeoCoordinateFrequencies(double* OmegaPhi, double* OmegaR, double p, double e);
 
 double get_separatrix(double a, double e, double x);
@@ -30,5 +34,13 @@ void KerrGeoConstantsOfMotion(double* E_out, double* L_out, double* Q_out, doubl
 
 double Y_to_xI(double a, double p, double e, double Y);
 void Y_to_xI_vector(double* x, double* a, double* p, double* e, double* Y, int length);
+
+void set_threads(int num_threads);
+int get_threads();
+
+double KerrGeoEnergy(double a, double p, double e, double x);
+double KerrGeoAngularMomentum(double a, double p, double e, double x, double En);
+
+double separatrix_KerrEquatorial(const double a, const double e);
 
 #endif // __FUND_FREQS__
