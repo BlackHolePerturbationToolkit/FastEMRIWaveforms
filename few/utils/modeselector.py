fcec59afd163bc8025f0eae8ad6dacdb4c6f46b0
Christian Chapman-Bird <christian.chapman-bird@ligo.org>
2023-11-03 18:12:33 +0000

    spheroidal mode selector

--- few/utils/modeselector.py
+++ few/utils/modeselector.py
@@ -239,6 +239,143 @@ class ModeSelector(ParallelModuleBase):
 
         return out1 + out2
 
+
+class GenericModeSelector(ParallelModuleBase):
+    """Filter teukolsky amplitudes based on power contribution.
+
+    This module takes teukolsky modes, combines them with their associated ylms,
+    and determines the power contribution from each mode. It then filters the
+    modes bases on the fractional accuracy on the total power (eps) parameter.
+    Additionally, if a sensitivity curve is provided, the mode power is also
+    weighted according to the PSD of the sensitivity.
+
+    The mode filtering is a major contributing factor to the speed of these
+    waveforms as it removes large numbers of useles modes from the final
+    summation calculation.
+
+    Be careful as this is built based on the construction that input mode arrays
+    will in order of :math:`m=0`, :math:`m>0`, and then :math:`m<0`.
+
+    args:
+        l_arr (1D int xp.ndarray): The l-mode indices for each mode index.
+        m_arr (1D int xp.ndarray): The m-mode indices for each mode index.
+        n_arr (1D int xp.ndarray): The n-mode indices for each mode index.
+        sensitivity_fn (object, optional): Sensitivity curve function that takes
+            a frequency (Hz) array as input and returns the Power Spectral Density (PSD)
+            of the sensitivity curve. Default is None. If this is not none, this
+            sennsitivity is used to weight the mode values when determining which
+            modes to keep. **Note**: if the sensitivity function is provided,
+            and GPUs are used, then this function must accept CuPy arrays as input.
+        **kwargs (dict, optional): Keyword arguments for the base classes:
+            :class:`few.utils.baseclasses.ParallelModuleBase`.
+            Default is {}.
+
+    """
+
+    def __init__(self, l_arr, m_arr, k_arr, n_arr, sensitivity_fn=None, **kwargs):
+        ParallelModuleBase.__init__(self, **kwargs)
+
+        if self.use_gpu:
+            xp = cp
+        else:
+            xp = np
+
+        self.sensitivity_fn = sensitivity_fn
+
+    @property
+    def gpu_capability(self):
+        """Confirms GPU capability"""
+        return True
+    
+    @property
+    def is_predictive(self):
+        """Whether this mode selector should be used before or after amplitude generation"""
+        return False
+    
+    def attributes_ModeSelector(self):
+        """
+        attributes:
+            xp: cupy or numpy depending on GPU usage.
+            num_m_zero_up (int): Number of modes with :math:`m\geq0`.
+            num_m_1_up (int): Number of modes with :math:`m\geq1`.
+            num_m0 (int): Number of modes with :math:`m=0`.
+            sensitivity_fn (object): sensitivity generating function for power-weighting.
+
+        """
+        pass
+
+    @property
+    def citation(self):
+        """Return citations related to this class."""
+        return larger_few_citation + few_citation + few_software_citation
+
+    def __call__(self, teuk_modes, modeinds, fund_freq_args=None, eps=1e-5):
+        """Call to sort and filer teukolsky modes.
+
+        This is the call function that takes the teukolsky modes, ylms,
+        mode indices and fractional accuracy of the total power and returns
+        filtered teukolsky modes and ylms.
+
+        args:
+            teuk_modes (2D complex128 xp.ndarray): Complex teukolsky amplitudes
+                from the amplitude modules.
+                Shape: (number of trajectory points, number of modes).
+            ylms (1D complex128 xp.ndarray): Array of ylm values for each mode,
+                including m<0. Shape is (num of m==0,) + (num of m>0,)
+                + (num of m<0). Number of m<0 and m>0 is the same, but they are
+                ordered as (m==0) first then m>0 then m<0.
+            modeinds (list of int xp.ndarrays): List containing the mode index arrays. If in an
+                equatorial model, need :math:`(l,m,n)` arrays. If generic,
+                :math:`(l,m,k,n)` arrays. e.g. [l_arr, m_arr, n_arr].
+            fund_freq_args (tuple, optional): Args necessary to determine
+                fundamental frequencies along trajectory. The tuple will represent
+                :math:`(M, a, e, p, \cos\iota)` where the large black hole mass (:math:`M`)
+                and spin (:math:`a`) are scalar and the other three quantities are xp.ndarrays.
+                This must be provided if sensitivity weighting is used. Default is None.
+            eps (double, optional): Fractional accuracy of the total power used
+                to determine the contributing modes. Lowering this value will
+                calculate more modes slower the waveform down, but generally
+                improving accuracy. Increasing this value removes modes from
+                consideration and can have a considerable affect on the speed of
+                the waveform, albeit at the cost of some accuracy (usually an
+                acceptable loss). Default that gives good mismatch qualities is
+                1e-5.
+
+        """
+
+        if self.use_gpu:
+            xp = cp
+        else:
+            xp = np
+
+        # get the power contribution of each mode, with R and L modes interleaved
+        power = (xp.abs(teuk_modes.reshape(teuk_modes.shape[0], -1)) ** 2)
+
+        # sort the power for a cumulative summation
+        inds_sort = xp.argsort(power, axis=1)[:, ::-1]
+        power = xp.sort(power, axis=1)[:, ::-1]
+        cumsum = xp.cumsum(power, axis=1)
+
+        # initialize and indices array for keeping modes
+        inds_keep = xp.full(cumsum.shape, True)
+
+        # keep modes that add to within the fractional power (1 - eps)
+        inds_keep[:, 1:] = cumsum[:, :-1] < cumsum[:, -1][:, xp.newaxis] * (1 - eps)
+
+        # finds indices of each mode to be kept
+        temp = inds_sort[inds_keep] // 2
+
+        # if R or L contributes, keep both due to structure of CUDA kernel
+        keep_modes = xp.unique(temp)
+
+        # setup up teuk mode returns
+        out1 = (teuk_modes[:, keep_modes],)
+
+        # setup up mode values that have been kept
+        out2 = tuple([arr[keep_modes] for arr in modeinds])
+
+        return out1 + out2
+
 class NeuralModeSelector(ParallelModuleBase):
     """Filter teukolsky amplitudes based on power contribution.
 
