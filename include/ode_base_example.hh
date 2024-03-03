03952caa8aa8828d54cc8f2b7cec2fdfd5ccd842
Christian Chapman-Bird <c.chapman-bird.1@research.gla.ac.uk>
2024-03-03 14:01:27 +0000

    schwarzecc no freqs

--- include/ode_base_example.hh
+++ include/ode_base_example.hh
@@ -30,6 +30,25 @@ public:
     ~SchwarzEccFlux();
 };
 
+class SchwarzEccFlux_nofrequencies
+{
+public:
+    interp_params *interps;
+    Interpolant *amp_vec_norm_interp;
+    double test;
+    bool convert_Y = false;
+    bool equatorial = true;
+    int background = SCHWARZSCHILD;
+    bool circular = false;
+    bool integrate_constants_of_motion = false;
+    bool integrate_phases = false;
+
+    SchwarzEccFlux_nofrequencies(std::string few_dir);
+
+    void deriv_func(double ydot[], const double y[], double epsilon, double a, double *additional_args);
+    ~SchwarzEccFlux_nofrequencies();
+};
+
 class KerrEccentricEquatorial
 {
 public:
