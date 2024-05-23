f3de34fda2b17283e0e88aad0b3d1386dc5db831
OllieBurke <ollie0burke@gmail.com>
2024-05-23 13:27:52 +0000

    checking other traj modules

--- integrate_backwards.py
+++ integrate_backwards.py
@@ -47,9 +47,9 @@ inspiral_kwargs = {
 M = 1e6
 mu = 1e1
 a = 0.85
-p_f = 3.5  # Final semi-latus rectum
-e_f = 0.15  # Final eccentricity
-iota0_f = 1.0  # Final inclination
+p_f = 2.75  # Final semi-latus rectum
+e_f = 0.0001  # Final eccentricity
+iota0_f = 0.8  # Final inclination
 Y_f = np.cos(iota0_f)
 T = 1 
 Phi_phi0 = 1.0
@@ -59,6 +59,7 @@ Phi_r0 = 3.0
 dt = 2.0
 
 p_sep = get_separatrix(a,e_f,1.0)
+print("Separatrix is", p_sep)
 # waveform_choice = input("Do you want Kerr inspirals? [y/n]")
 # if waveform_choice == "y":
 #     waveform_class = 'Pn5AAKWaveform'
@@ -70,13 +71,14 @@ p_sep = get_separatrix(a,e_f,1.0)
 #     print("Eccentric Schwarzschild trajectory and waveform")
 
 # trajectory_class = "pn5"
+# trajectory_class = "pn5_nofrequencies"
 # trajectory_class = "SchwarzEccFlux"
 trajectory_class = "KerrEccentricEquatorial"
+# trajectory_class = "KerrEccentricEquatorial_nofrequencies"
+# trajectory_class = "KerrEccentricEquatorial_ELQ"
 
-if trajectory_class == "SchwarzEccFlux" or trajectory_class == "KerrEccentricEquatorial":
-    Y_f = 1.0 
-    if trajectory_class == "SchwarzEccFlux":
-        a = 0.0
+Y_f = 1.0
+# a = 0.0
 
 inspiral_kwargs_back = {
     "DENSE_STEPPING": 0,  # Set to 1 if we want a sparsely sampled trajectory
@@ -93,13 +95,15 @@ inspiral_kwargs_forward = {
 }
 
 
-
+import time
 # Set up trajectory module for backwards integration
 traj_backwards = EMRIInspiral(func = trajectory_class)
 
 # Generate backwards integration
+start = time.time()
 t_back, p_back, e_back, Y_back, Phi_phi_back, Phi_r_back, Phi_theta_back = traj_backwards(M, mu, a, p_f, e_f, Y_f, 
                                              Phi_phi0=Phi_phi0, Phi_theta0=Phi_theta0, Phi_r0=Phi_r0, dt = dt, T=T, **inspiral_kwargs_back)
+print("Integrating backwards took", time.time() - start, "seconds")
 # Set up trajectory module for forwards integration
 traj_forwards = EMRIInspiral(func = trajectory_class)
 
@@ -111,12 +115,18 @@ print("Initial parameters would be:")
 print("p = ",initial_p)
 print("e = ",initial_e)
 print("Y = ",initial_Y)
+print("New separatrix is p_sep = ", get_separatrix(a,initial_e, 1.0))
 print("")
 
 # Generate forwards integration
+start = time.time()
 t_forward, p_forward, e_forward, Y_forward, Phi_phi_forward, Phi_r_forward, Phi_theta_forward = traj_forwards(M, mu, a, initial_p, initial_e, initial_Y, 
                                              Phi_phi0=Phi_phi0, Phi_theta0=Phi_theta0, Phi_r0=Phi_r0, T=T, **inspiral_kwargs_forward)
+print("Integrating forwards took", time.time() - start, "seconds")
 print("Finished trajectories")
+breakpoint()
+quit()
+
 p_back_interp = interp1d(max(t_back) - t_back,p_back, kind = 'cubic')
 p_check_forward = p_back_interp(t_forward)
 # ======================== WAVEFORM GENERATION =============================
@@ -156,7 +166,6 @@ sum_kwargs = {
 # Generate waveform using backwards integration
 # waveform_class = 'Pn5AAKWaveform'
 # waveform_class = 'FastSchwarzschildEccentricFlux'
-waveform_class = "KerrEccentricEquatorialFlux"
 wave_generator_backwards = GenerateEMRIWaveform(waveform_class, 
                                                 inspiral_kwargs = inspiral_kwargs_back,
                                                 sum_kwargs = sum_kwargs,
@@ -195,20 +204,36 @@ h_p_forward_int = cp.asnumpy(waveform_forward.real)
 n_t = len(h_p_forward_int)
 t = np.arange(0,n_t*dt,dt)
 
-# Plot the two waveforms against eachother
-plot_direc = "/home/ad/burkeol/work/Current_Projects/FastEMRIWaveforms_backwards/waveform_plots"
-plt.plot(t/24/60/60,h_p_back_int[::-1], c = 'blue', alpha = 1, linestyle = 'dashed', label = 'integrated backwards')
-plt.plot(t[0:-1]/24/60/60,h_p_forward_int[1:], c = 'red', alpha = 0.5, label = 'integrated forwards')
-title_str = r'Kerr: $(M, \mu, a, p_f, e_f, T) = (1e6, {}, {}, {}, {}, {} \, \text{{year}})$'.format(mu,a, p_f, e_f, 1)
-plt.title(title_str, fontsize = 12)
-plt.xlabel(r'Time $t$ [days]')
-plt.ylabel(r'$h_{p}$')
-# plt.xlim([365-0.01,365])
-plt.xlim([365 - 0.01,365])
-plt.legend()
-plt.grid()
-plt.show()
-plt.savefig(plot_direc + "/waveform_back_forward_Kerr.pdf", bbox_inches="tight")
-plt.clf()
+# Compute mismatches between waveforms
 
-breakpoint()
+h_p_forward_fft = cp.fft.rfft(cp.roll(waveform_forward.real,-1))
+h_p_back_reversed_fft = cp.fft.rfft(waveform_back.real[::-1])
+
+def inner_prod(sig1_f,sig2_f):
+    """
+    Filthy overlap. No noise weighting. Just RAAAW signal. 
+    """
+    ab = cp.sum(sig1_f*sig2_f.conj())
+    aa = cp.sum(cp.abs(sig1_f)**2)
+    bb = cp.sum(cp.abs(sig2_f)**2)
+
+    return np.real(ab/cp.sqrt(aa*bb))
+
+overlap = inner_prod(h_p_forward_fft,h_p_back_reversed_fft)
+mismatch = 1 - overlap
+print("Mismatch is", 1 - overlap)
+# Plot the two waveforms against eachother
+# plot_direc = "/home/ad/burkeol/work/Current_Projects/FastEMRIWaveforms_backwards/waveform_plots"
+# plt.plot(t/24/60/60,h_p_back_int[::-1], c = 'blue', alpha = 1, linestyle = 'dashed', label = 'integrated backwards')
+# plt.plot(t[0:-1]/24/60/60,h_p_forward_int[1:], c = 'red', alpha = 0.5, label = 'integrated forwards')
+# title_str = r'Kerr: $(M, \mu, a, p_f, e_f, T) = (1e6, {}, {}, {}, {}, {} \, \text{{year}})$'.format(mu,a, np.round(p_f,3), e_f, 1)
+# plt.title(title_str, fontsize = 12)
+# plt.xlabel(r'Time $t$ [days]')
+# plt.ylabel(r'$h_{p}$')
+# # plt.xlim([365-0.01,365])
+# plt.xlim([365 - 0.01,365])
+# plt.legend()
+# plt.grid()
+# plt.show()
+# plt.savefig(plot_direc + "/waveform_back_forward_Kerr.pdf", bbox_inches="tight")
+# plt.clf()
