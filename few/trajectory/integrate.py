b7a617b226e2cbfb56e582133aa5b074b78d5877
Christian Chapman-Bird <c.chapmanbird@gmail.com>
2024-05-28 21:42:33 +0100

    separatrix rootfinder and interpolant

--- few/trajectory/integrate.py
+++ few/trajectory/integrate.py
@@ -26,13 +26,14 @@ import time
 import numpy as np
 from scipy.interpolate import CubicSpline
 from scipy.integrate import solve_ivp
+from scipy.optimize import brentq
 
 # Cython/C++ imports
 from pyInspiral import pyInspiralGenerator, pyDerivative
 
 # Python imports
 from ..utils.baseclasses import TrajectoryBase
-from ..utils.utility import check_for_file_download, get_ode_function_options, ELQ_to_pex, get_kerr_geo_constants_of_motion
+from ..utils.utility import check_for_file_download, get_ode_function_options, ELQ_to_pex, get_kerr_geo_constants_of_motion, get_separatrix_interpolant
 from ..utils.constants import *
 from ..utils.citations import *
 
@@ -51,7 +52,7 @@ from ..utils.utility import get_separatrix
 # sep = ( get_sep(XYZ[0].flatten(), XYZ[1].flatten(), XYZ[2].flatten())).reshape((100,100,100))
 # get_separatrix = TricubicSpline(a, e, x, sep)
 
-DIST_TO_SEPARATRIX = 0.1
+DIST_TO_SEPARATRIX = 0.05
 INNER_THRESHOLD = 1e-8
 PERCENT_STEP = 0.25
 MAX_ITER = 1000
@@ -59,7 +60,7 @@ MAX_ITER = 1000
 KERR = 1
 SCHWARZSCHILD = 2
 
-def get_integrator(func, nparams, few_dir):
+def get_integrator(func, nparams, few_dir, **kwargs):
 
     ode_info = get_ode_function_options()
     
@@ -84,9 +85,9 @@ def get_integrator(func, nparams, few_dir):
     if not integrator.integrate_phases[0]:
         nparams -= 3
     if integrator.integrate_constants_of_motion[0]:
-        return AELQIntegrate(func, nparams, few_dir, num_add_args=num_add_args)
+        return AELQIntegrate(func, nparams, few_dir, num_add_args=num_add_args,**kwargs)
     else:
-        return APEXIntegrate(func, nparams, few_dir, num_add_args=num_add_args)
+        return APEXIntegrate(func, nparams, few_dir, num_add_args=num_add_args,**kwargs)
 
 def fun_wrap(t, y, integrator):
     # print("YEYA", t, y)
@@ -122,14 +123,21 @@ class Integrate:
         few_dir: str,
         buffer_length: int = 1000,
         num_add_args: int = 0,
+        rootfind_separatrix = True,
     ):
         self.buffer_length = buffer_length
         self.integrator = pyInspiralGenerator(
             nparams,
             num_add_args,
         )
+        self.dense_integrator = pyInspiralGenerator(
+            nparams,
+            num_add_args,
+        )
         self.ode_info = ode_info = get_ode_function_options()
         self.few_dir = few_dir
+
+        self.rootfind_separatrix = rootfind_separatrix
 
         if isinstance(func, str):
             func = [func]
@@ -141,6 +149,7 @@ class Integrate:
                     f"func not available. Options are {list(ode_info.keys())}."
                 )
             self.integrator.add_ode(func_i.encode(), few_dir.encode())
+            self.dense_integrator.add_ode(func_i.encode(), few_dir.encode())
 
             # make sure all files needed for the ode specifically are downloaded
             for fp in ode_info[func_i]["files"]:
@@ -207,6 +216,7 @@ class Integrate:
                     f"func not available. Options are {list(ode_info.keys())}."
                 )
             self.integrator.add_ode(func_i.encode(), self.few_dir.encode())
+            self.dense_integrator.add_ode(func_i.encode(), self.few_dir.encode())
 
             # make sure all files needed for the ode specifically are downloaded
             for fp in ode_info[func_i]["files"]:
@@ -312,8 +322,10 @@ class Integrate:
 
     def initialize_integrator(self, err=1e-10, DENSE_STEPPING=False, use_rk4=False, **kwargs):
         self.integrator.set_integrator_kwargs(err, DENSE_STEPPING, not use_rk4)
+        self.dense_integrator.set_integrator_kwargs(err, True, not use_rk4)  # always dense step for final stage
 
         self.integrator.initialize_integrator()
+        self.dense_integrator.initialize_integrator()
         self.trajectory_arr = np.zeros((self.buffer_length, self.nparams + 1))
         self.traj_step = 0
     
@@ -349,10 +361,13 @@ class Integrate:
         )
 
         self.integrator.add_parameters_to_holder(M, mu, a, additional_args)
+        self.dense_integrator.add_parameters_to_holder(M, mu, a, additional_args)
+
         t0 = 0.0
         self.integrate(t0, y0)
 
         self.integrator.destroy_integrator_information()
+        self.dense_integrator.destroy_integrator_information()
 
         return self.trajectory
 
@@ -367,8 +382,8 @@ class APEXIntegrate(Integrate):
             p_sep = 6.0 + 2.0 * e
 
         else:
-            p_sep = get_separatrix(self.a, e, x)
-
+            # p_sep = get_separatrix(self.a, e, x)
+            p_sep = get_separatrix_interpolant(self.a, e, x)
         return p_sep
 
     def action_function(
@@ -404,7 +419,7 @@ class APEXIntegrate(Integrate):
 
         return (temp_t, temp_y, temp_stop)
 
-    def finishing_function(self, t: float, y: np.ndarray):
+    def finishing_function_euler_step(self, t: float, y: np.ndarray):
         # Issue with likelihood computation if this step ends at an arbitrary value inside separatrix + DIST_TO_SEPARATRIX.
         #     // To correct for this we self-integrate from the second-to-last point in the integation to
         #     // within the INNER_THRESHOLD with respect to separatrix +  DIST_TO_SEPARATRIX
@@ -443,6 +458,58 @@ class APEXIntegrate(Integrate):
                     )
 
             self.save_point(t * self.Msec, y)
+
+    def inner_func(self, h, t, y, y_out):
+        y_out[:] = y[:].copy()
+
+        # careful here, if the step is too big it will return a ValueError. One to watch out for in future
+        try:
+            status, self.t_final_temp, self.h_final_temp = self.dense_integrator.take_step(t, h, y_out, self.tmax_dimensionless)
+        except ValueError:
+            return -np.inf  # for the root finder
+
+        p_sep = self.get_p_sep(y_out)
+
+        # edge effect check: stops the loop if we cross the maximum time during this tuning process
+        # this may be disabled for performance? probably negligible cost to trajectory as only if statements
+        if self.t_final_temp > self.tmax_dimensionless:
+            if y_out[0] > p_sep + DIST_TO_SEPARATRIX:
+                final_h = self.h_final_temp - (self.t_final_temp - self.tmax_dimensionless)
+                y_out[:] = y[:].copy()
+
+                status, self.t_final_temp, self.h_final_temp = self.dense_integrator.take_step(t, final_h, y_out, self.tmax_dimensionless)
+
+                raise ValueError("We need to stop - hit the maximum time before the separatrix.")
+
+        return y_out[0] - (p_sep + DIST_TO_SEPARATRIX)  # we want this to go to zero
+
+    def finishing_function(self, t: float, y: np.ndarray):
+        # Tune the step-size of a dense integration step such that the trajectory terminates within INNER_THRESHOLD of p_sep + DIST_TO_SEPARATRIX 
+
+        if t < self.tmax_dimensionless:  # don't step to the separatrix if we already hit the time window
+            if self.rootfind_separatrix:  # use a root-finder and the full integration routine to tune the finish
+                p_sep = self.get_p_sep(y)
+                p = y[0]
+                ydot = self.dense_integrator.get_derivatives(y)
+                pdot = ydot[0]
+                y_temp = y.copy()
+                y_out = y_temp.copy()
+
+                step_size = (p_sep + DIST_TO_SEPARATRIX - p) / pdot  # roughly work out the scale of the step
+
+                try:
+                    result = brentq(self.inner_func, step_size / 2, step_size * 2, args=(t, y_temp, y_out),maxiter=MAX_ITER, xtol=INNER_THRESHOLD, rtol=1e-8, full_output=True)
+
+                    if result[1].converged:
+                        self.save_point(self.t_final_temp * self.Msec, y_out)
+                    else:
+                        raise RuntimeError("Separatrix root-finding operation did not converge within MAX_ITER.")
+
+                except ValueError as e:  # for the edge case where t -> tmax while we are tuning this
+                    self.save_point(self.t_final_temp * self.Msec, y_out)
+
+            else:
+                self.finishing_function_euler_step(t, y)  # weird step-halving Euler thing
 
 class AELQIntegrate(Integrate):
     def get_p_sep(self, y: np.ndarray) -> float:
