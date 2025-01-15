c2ef5307d9457651a349c3d61bb810f5f1958999
Maxime PIGOU <maxime.pigou@l2it.in2p3.fr>
2025-01-15 09:45:11 +0000

    feat: Add CLI `few_citations` tool to get a class citation from command-line

--- src/few/utils/citations.py
+++ src/few/utils/citations.py
@@ -266,3 +266,42 @@ class Citable:
     def module_references(cls) -> list[REFERENCE]:
         """Method implemented by each class to define its list of references"""
         return COMMON_REFERENCES
+
+
+def cli_citation():
+    """Add CLI utility to retrieve the citation of a given class."""
+    import argparse
+    import importlib
+    import sys
+    parser = argparse.ArgumentParser(
+        prog='few_citations',
+        description='Export the citations associated to a given module of the FastEMRIWaveforms package',
+    )
+    parser.add_argument('module')
+    args = parser.parse_args(sys.argv[1:])
+
+    few_class: str = args.module
+
+    if not few_class.startswith('few.'):
+        raise ValueError("The requested class must be part of the 'few' package (e.g. 'few.amplitude.AmpInterp2D').")
+
+    module_path, class_name = few_class.rsplit('.', 1)
+
+    try:
+        module = importlib.import_module(module_path)
+    except ModuleNotFoundError as e:
+        raise ImportError("Could not import module '{}'.".format(module_path)) from e
+
+    try:
+        class_ref = getattr(module, class_name)
+    except AttributeError as e:
+        raise ImportError("Could not import class '{}' (not found in module '{}')".format(class_name, module_path)) from e
+
+    if not issubclass(class_ref, Citable):
+        print("Class '{}' ".format(few_class) + "does not implement specific references.\n"
+              "However, since you are using the FastEMRIWaveform software, "
+              "you may cite the following references: \n")
+        print(Citable.citation())
+        return
+
+    print(class_ref.citation())
