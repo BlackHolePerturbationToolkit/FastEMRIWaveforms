0c4da13b0d6ed2e6318520ff3062268260f2d201
OllieBurke <ollie0burke@gmail.com>
2025-03-06 14:37:30 +0000

    eradication of useless constants

--- src/few/utils/constants.py
+++ src/few/utils/constants.py
@@ -6,5 +6,5 @@ YRSID_SI = ASTRONOMICAL_YEAR # Number of seconds in one astronomical year
 MTSUN_SI = GM_SUN / (SPEED_OF_LIGHT**3)  # One solar mass in seconds 
 MRSUN_SI = 0.5 * SUN_SCHWARZSCHILD_RADIUS  # Schwarzschild radius in meters
 Gpc = 1e9 * PARSEC            # One Gpc in meters
-PI = 3.141592653589793238462643383279502884   # Value of Pi... 
+PI = 3.141592653589793238462643383279502884   # Value of Pi 
 
