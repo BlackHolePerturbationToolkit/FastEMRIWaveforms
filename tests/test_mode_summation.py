2f49dc5cb4bfab9e8dbfdf12e195ace128983eb1
Christian Chapman-Bird <c.chapmanbird@gmail.com>
2025-03-09 14:07:13 +0000

    interpolatedmodesum test, ensure some types in interpolatedmodesum

--- tests/test_mode_summation.py
+++ tests/test_mode_summation.py
@@ -2,8 +2,9 @@ import unittest
 import numpy as np
 
 from few.summation.interpolatedmodesum import CubicSplineInterpolant, InterpolatedModeSum
-from scipy.interpolate import CubicSpline
+from scipy.interpolate import CubicSpline, interp1d
 from few.summation.directmodesum import DirectModeSum
+from few.trajectory.inspiral import EMRIInspiral
 from few.utils.globals import get_logger, get_first_backend
 
 few_logger = get_logger()
@@ -50,6 +51,51 @@ class SummationTest(unittest.TestCase):
     def test_interpolated_mode_sum(self):
         # check that the interpolatedmodesum accurately computes a dummy case
         summation = InterpolatedModeSum(force_backend=best_backend)
+
+        # interpolatedmodesum expects 8th-order phase coefficients of a particular form
+        # the easiest way to obtain these is to just evolve a 0PA trajectory
+        # in the future when our integrator is fully stand-alone this can be generalised
+        traj_module = EMRIInspiral(func="KerrEccEqFlux")
+        traj = traj_module(1e6, 1e1, 0.4, 20., 0.6, 1., T=0.1, err=1e-15)
+        t_spl = traj_module.inspiral_generator.integrator_t_cache
+        coeff_spl = traj_module.inspiral_generator.integrator_spline_coeff[:,[3,5],:] / (1e1 / 1e6)
+
+        t_eval = np.linspace(0, t_spl[-1], 10001)
+        dt = t_eval[1] - t_eval[0]
+        phases_eval = traj_module.inspiral_generator.eval_integrator_spline(t_eval)[:,[3,5]].T
+
+        # now we make up the rest
+        num_modes = 10
+
+        # dummy amplitudes: linearly ramping in real and imaginary part
+        amp_temp = np.linspace(np.random.uniform(-1, 1, (2, num_modes)), np.random.uniform(-1, 1, (2,num_modes)), t_eval.size, axis=-1)
+        amplitude = (amp_temp[0] + 1j*amp_temp[1]).T
+
+        # we need to provide these values at the spline knots for InterpolatedModeSum
+        # easy to do here as we are just ramping them linearly
+        amplitude_spl_temp = interp1d(t_eval, amp_temp, axis=-1, kind='linear', assume_sorted=True)(t_spl)
+        amplitude_spl = (amplitude_spl_temp[0] + 1j*amplitude_spl_temp[1]).T
+
+        # dummy mode indices
+        l_arr = np.ones(num_modes) * 2  # not used in this fictitious summation
+        m_arr = np.random.randint(2, 5, num_modes)
+        n_arr = np.random.randint(-4, 4, num_modes)
+
+        # dummy 'spherical harmonics'
+        ylms = np.ones(2 * num_modes)
+        ylms[num_modes:] = 0   # disable the mode symmetry
+
+        # perform the summation manually
+        mode_phase_values = phases_eval[0,:,None] * m_arr[None,:] + phases_eval[1,:,None] * n_arr[None,:]
+        phasors = amplitude * np.exp(-1j * mode_phase_values)
+        manual_sum = phasors.sum(-1)
+
+        few_sum = summation(t_spl, amplitude_spl, ylms, t_spl, coeff_spl, l_arr, m_arr, n_arr, dt=dt)
+
+        if best_backend.uses_gpu:
+            few_sum = few_sum.get()
+
+        np.testing.assert_allclose(manual_sum, few_sum, rtol=1e-10)
 
     def test_direct_mode_sum(self):
         # check that the directmodesum accurately computes a dummy case
