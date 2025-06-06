f8975aeeeb28e7b0bc06f17c79d772f3c6207dcc
Maxime PIGOU <maxime.pigou@l2it.in2p3.fr>
2025-06-06 14:59:29 +0000

    chore: remove remaining GPL license headers

--- src/few/utils/baseclasses.py
+++ src/few/utils/baseclasses.py
@@ -1,20 +1,5 @@
 # Collection of base classes for FastEMRIWaveforms Packages
 
-# Copyright (C) 2020 Michael L. Katz, Alvin J.K. Chua, Niels Warburton, Scott A. Hughes
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 """
 The :code:`few.utils.baseclasses` module contains abstract base classes for the
 various modules. When creating new modules, these classes should be used to maintain
