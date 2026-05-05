f5d642a2b49cb5969e1062dcf8f1ad22bdc87e03
Christian Chapman-Bird <c.chapmanbird@gmail.com>
2026-05-05 11:41:48 +0100

    fix typos from previous PR

--- src/few/trajectory/ode/flux.py
+++ src/few/trajectory/ode/flux.py
@@ -126,17 +126,17 @@ class SchwarzEccFlux(ODEBase):
     def max_p(self, e, x=1, a=0):
         return PMAX_SCHW + 2.0 * e
 
-    def bounds_p(self, e, x=1, a=0, p_buffer=[0, 0], separatrix_buffer = None):
+    def bounds_p(self, e, x=1, a=0, p_buffer=[0, 0], separatrix_buffer = None, max_e_buffer = None):
         return [self.min_p(e, x, a, separatrix_buffer=separatrix_buffer) + p_buffer[0], self.max_p(e, x, a) - p_buffer[1]]
 
     def max_e(self, p, x=1, a=0):
         return EMAX_SCHW
 
-    def isvalid_pex(self, p=20, e=0, x=1, a=0, p_buffer=[0, 0], e_buffer=[0, 0], sepatrix_buffer = None):
+    def isvalid_pex(self, p=20, e=0, x=1, a=0, p_buffer=[0, 0], e_buffer=[0, 0], separatrix_buffer = None, max_e_buffer = None):
         self.isvalid_x(x)
         self.isvalid_e(e, e_buffer=e_buffer)
         self.isvalid_a(a)
-        pmin, pmax = self.bounds_p(e, x, a, p_buffer=p_buffer, separatrix_buffer=sepatrix_buffer)
+        pmin, pmax = self.bounds_p(e, x, a, p_buffer=p_buffer, separatrix_buffer=separatrix_buffer, max_e_buffer=max_e_buffer)
         assert p >= pmin and p <= pmax, (
             f"Interpolation: p out of bounds. Must be between {pmin + p_buffer[0]} and {pmax - p_buffer[1]}."
         )
